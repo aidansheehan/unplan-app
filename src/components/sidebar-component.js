de07844ac9b0b2e8834a1b6749c2644d2d04e250
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-12-30 16:20:22 +0000

    navbar new: better navbar

---
+++ src/components/sidebar-component.js
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+  // Sidebar component
+  const SidebarComponent = ({navItems}) => {
+
+    const router = useRouter()
+
+    // Define a state to track the active page
+    const [ activePage, setActivePage ] = useState('')
+
+    useEffect(() => {
+      setActivePage(router.pathname)
+    }, [router])
+
+    return (
+<aside className="bg-blue-600 p-4 row-start-2 row-end-3 col-start-1 flex flex-col justify-center">
+  <nav>
+    <ul className="space-y-4">
+      {navItems.map(item => (
+        <li key={item.href} className={`rounded-md transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-md ${activePage === item.href ? 'bg-blue-700 text-white' : 'hover:bg-blue-500 text-white'}`}>
+          <Link href={item.href} className="flex items-center space-x-2 p-3">
+            <FontAwesomeIcon icon={item.icon} fixedWidth />
+            <span className="text-lg">{item.text}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+</aside>
+
+
+    )
+};
+
+export default SidebarComponent
