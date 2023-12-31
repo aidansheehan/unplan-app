a307e9345fbb04583b199ac01cb3a69584c5e641
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-12-31 12:36:49 +0000

    prevent auto laod CSS to stop font awesome icons flashing big

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 
+//Fix for issue https://stackoverflow.com/questions/56334381/why-my-font-awesome-icons-are-being-displayed-big-at-first-and-then-updated-to-t#:~:text=This%20is%20a%20very%20common,before%20the%20CSS%20is%20loaded
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
