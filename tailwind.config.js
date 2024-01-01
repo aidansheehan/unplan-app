007e94dcfd47c0a53c01ea78c853f83cda316161
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2024-01-01 17:40:48 +0000

    style-tweaks new: fall back to monospace cos looks better

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        'permanent-marker': ['Permanent Marker', 'cursive'],
+        'permanent-marker': [ 'Permanent Marker', 'monospace'],
         'bree': ['Bree Serif', 'serif']
 
       }
