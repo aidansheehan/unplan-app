bcbb4a9d38cff55d5fae9499aafd641454a20061
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2024-01-02 22:02:50 +0000

    windows-styling new: overide xl to specifically target retina screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
       },
       boxShadow: {
         'bottom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
+      },
+      screens: {
+        'xl': '1440px'
       }
     },
   },
