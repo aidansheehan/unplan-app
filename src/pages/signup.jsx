6a501ada5db85e62609f5308bed3250dd3a141cb
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2024-02-19 20:52:39 +0000

    auth new: redirect authenticated users from login/signup to home, remove router push on signout as handled by route guard logic, use useEffect and ready state for route guarding as more react friendly and better ux (no flash)

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout'
 import SignupFormComponent from '@/components/signup-form.component'
+import RedirectIfAuthenticated from '@/hoc/redirect-if-authenticated'
 
 /**
  * Component to sign up
@@ -13,4 +14,4 @@ const SignUp = () => {
     )
 }
 
-export default SignUp
+export default RedirectIfAuthenticated(SignUp)
