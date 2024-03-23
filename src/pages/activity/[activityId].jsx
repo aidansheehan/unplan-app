8dfff22075b9b2b4b7715aca9f256941d34a98bc
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2024-03-23 11:43:03 +0000

    firebase-rules new: fix activities

--- src/pages/activity/[activityId].jsx
+++ src/pages/activity/[activityId].jsx
@@ -1,7 +1,4 @@
-import { faFrownOpen } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { doc, getDoc } from "firebase/firestore"
-import Link from "next/link"
 import { db } from "../../../firebaseConfig"
 import ACTIVITY_INFO from "@/constants/activity-info.constant"
 import ActivityInstructionsComponent from "@/components/activity-instructions.component"
@@ -10,11 +7,23 @@ import LoadingSpinner from "@/components/loading-spinner"
 import TinyMceEditor from "@/components/tinymce-editor.component"
 import ProtectedRoute from "@/hoc/protected-route.hoc"
 import PageHeaderComponent from "@/components/page-header"
+import { useActivities } from "@/context/activities.context"
+import { useRouter } from "next/router"
 
 /**
  * Page to view & print a generated activity
  */
-const ViewActivity = ({worksheetUrl, activity, topic, activityId, error}) => {
+const ViewActivity = ({activityId}) => {
+
+    const { activities }    = useActivities()
+    const router            = useRouter()
+    const activityData      = activities.find(a => a.id === activityId)
+
+    if (!activityData) {
+        router.replace('/not-found')
+    }
+
+    const { worksheetUrl, activity, topic } = activityData
 
     const [ worksheetLoading, setWorksheetLoading ] = useState(true)
     const [ worksheetContent, setWorksheetContent ] = useState('')
@@ -45,22 +54,6 @@ const ViewActivity = ({worksheetUrl, activity, topic, activityId, error}) => {
 
     }, [ worksheetUrl ])
 
-    //Handle error
-    if (error) {
-        return (
-                <div className="w-full h-full p-4 text-center">
-                    <FontAwesomeIcon icon={faFrownOpen} size="3x" className="text-orange-500 mb-4" />
-                    <h2 className="text-2xl font-bold text-blue-900 mb-4">Oops! Activity not found.</h2>
-                    <p className="text-lg text-blue-700 mb-6">
-                        We couldn't find the activity you're looking for. It might have been removed or the link could be incorrect.
-                    </p>
-                    <Link href="/" className="bg-orange-500 text-white font-bold py-3 px-6 rounded-lg hover:bg-orange-600 transition-colors duration-300">
-                            Go Back to Home
-                    </Link>
-                </div>
-        )
-    }
-
     return (
         <>
 
@@ -87,6 +80,12 @@ export async function getServerSideProps(context) {
 
     //Get activity ID from params
     const { activityId } = context.params
+
+    return {
+        props: {
+            activityId
+        }
+    }
 
     try {
 
